d8b9991c58e5dda2da36d8a7bfe45a55ce1c7b14
azizn <aziznemlaghi20@gmail.com>
2023-07-07 11:42:41 +0100

    Initial commit

---
+++ Crud application/Data/Crud_applicationContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Crud_application.Model;
+
+namespace Crud_application.Data
+{
+    public class Crud_applicationContext : DbContext
+    {
+        public Crud_applicationContext (DbContextOptions<Crud_applicationContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Crud_application.Model.User> User { get; set; } = default!;
+    }
+}
---
+++ Crud application/Pages/Users/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Crud_application.Data;
+using Crud_application.Model;
+
+namespace Crud_application.Pages.Users
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Crud_application.Data.Crud_applicationContext _context;
+
+        public IndexModel(Crud_application.Data.Crud_applicationContext context)
+        {
+            _context = context;
+        }
+
+        public IList<User> User { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.User != null)
+            {
+                User = await _context.User.ToListAsync();
+            }
+        }
+    }
+}
